bc19037cf1adf34299ee61cad31d1aa45a448b10
Remuraa <renanuemura@hotmail.com>
2019-08-26 22:54:38 -0300

    Projeto Criado

---
+++ src/main/java/com/example/demo/CursoUdemy1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoUdemy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoUdemy1Application.class, args);
+	}
+
+}
